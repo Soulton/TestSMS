c69d04b5c106bff2524254d5a5494d91ea954b01
Soulton <40466162+Soulton@users.noreply.github.com>
2021-03-11 18:20:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 import {sms} from './TEST.js';
-sms();
+
 
 const express = require("express");
 const app = express();
 
-app.get('/', function(request, response) {
-    response.send('Hello World!');
+app.use('/',function (request, response) {
+    response.send(`<!DOCTYPE html>
+  <html>
+  <button onclick='sms()' >TestSMS</button>
+  <html>`);
 });
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, function() {
